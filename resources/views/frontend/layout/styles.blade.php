c4d34afaa7cf623538a80ff4addf07de9a814fb8
nafisnil <nafis.zaman1.an@gmail.com>
2024-01-25 15:10:13 +0600

    frontend_contd

---
+++ resources/views/frontend/layout/styles.blade.php
@@ -0,0 +1,7 @@
+
+<link rel="stylesheet" href="{{ asset('dist_frontend') }}/css/bootstrap.min.css">
+<link rel="stylesheet" href="{{ asset('dist_frontend') }}/css/all.css">
+<link rel="stylesheet" href="{{ asset('dist_frontend') }}/css/owl.carousel.min.css">
+<link rel="stylesheet" href="{{ asset('dist_frontend') }}/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="{{ asset('dist_frontend') }}/css/animate.css">
+<link rel="stylesheet" href="{{ asset('dist_frontend') }}/css/custom.css">
