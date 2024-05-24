55b974b367f1f29818e2a967282d497cdcca5c66
nafisnil <nafis.zaman1.an@gmail.com>
2024-05-25 01:50:30 +0600

    color

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -12,13 +12,29 @@
     <link rel="icon" type="image/png" href="{{ asset('uploads/'.$global_setting_data->favicon) }}">
     <meta name="description" value="@yield('seo_meta_description')">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <style>
+        nav .nav-item .nav-link.active,
+        nav .nav-item .nav-link:hover,
+        .home-banner .left .button a,
+        .home-about .right h3,
+        .home-about .contact-info i{
+            color: {{ $global_setting_data->theme_color }}
+        }
 
+        .home-banner,
+        .home-about .social ul li{
+            background: {{ $global_setting_data->theme_color }}
+        }
+    </style>
 </head>
 <body>
 
+    @if ($global_setting_data->preloader_status == 'Show')
     <div id="preloader">
         <div id="preloader_inner"></div>
     </div>
+    @endif
+
 
 @include('frontend.layout.nav')
 
@@ -37,14 +53,14 @@
                     </div>
                     <div class="social">
                         <ul>
-                            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href=""><i class="fab fa-twitter"></i></a></li>
-                            <li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
-                            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="{{ @$global_setting_data->footer_icon_1_url }}"><i class="{{ @$global_setting_data->footer_icon_1 }}"></i></a></li>
+                            <li><a href="{{ @$global_setting_data->footer_icon_2_url }}"><i class="{{ @$global_setting_data->footer_icon_2 }}"></i></a></li>
+                            <li><a href="{{ @$global_setting_data->footer_icon_3_url }}"><i class="{{ @$global_setting_data->footer_icon_3 }}"></i></a></li>
+                            <li><a href="{{ @$global_setting_data->footer_icon_4_url }}"><i class="{{ @$global_setting_data->footer_icon_4 }}"></i></a></li>
                         </ul>
                     </div>
                     <div class="copyright">
-                        Copright 2022, company name. All Rights Reserved.
+                        {{ @$global_setting_data->footer_copyright_text }}
                     </div>
                 </div>
             </div>
